a1c596d9bc58672027aabd5dcfd2bc4fde543fec
aschelstraete <aschelstraete@gmail.com>
2017-06-15 18:48:08 -0300

    Added vimeo background and animations

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+// Video background
+
+var elem = document.getElementById("videoBgr");//based on Fabrizio's comment declare it outside
+setTimeout(function(){
+   elem.className = elem.className + "video-fade";
+},1000);
+
+var timeoutId;
+var $videoBgAspect = $(".video-aspect");
+var $videoBgWidth = $(".video-width");
+var videoAspect = $videoBgAspect.outerHeight() / $videoBgAspect.outerWidth();
+
+function videobgEnlarge() {
+  console.log('resize');
+  windowAspect = ($(window).height() / $(window).width());
+  if (windowAspect > videoAspect) {
+    $videoBgWidth.width((windowAspect / videoAspect) * 100 + '%');
+  } else {
+    $videoBgWidth.width(100 + "%")
+  }
+}
+
+$(window).resize(function() {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(videobgEnlarge, 100);
+});
+
+$(function() {
+  videobgEnlarge();
+});
